4a2517b11b44b8562ca7b81dbf064d01ae1f049f
AppleLee0423 <ld210@naver.com>
2022-11-11 00:06:51 +0900

    First commit

---
+++ src/main/java/com/spring/namyang/ProjectNamyangApplication.java
@@ -0,0 +1,13 @@
+package com.spring.namyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNamyangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNamyangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/namyang/ProjectNamyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.namyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNamyangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
